fae5e85c7161838a45c6cd3fa631cbf89f62778d
Mikail Khan <mikail@mikail-khan.com>
2021-07-22 17:54:59 -0400

    stupid rebase

--- xmonad.hs
+++ xmonad.hs
@@ -10,6 +10,13 @@ import XMonad.Hooks.InsertPosition
 import qualified Data.Map as M
 import qualified XMonad.StackSet as W
 
+import XMonad.Util.Run
+import XMonad.Util.WorkspaceCompare
+import XMonad.Hooks.ManageDocks
+import XMonad.Hooks.DynamicLog
+
+import XMonad.Hooks.EwmhDesktops
+
 centerRect = W.RationalRect 0.25 0.25 0.5 0.5
 
 doIfFocusedIsFloating a b = withFocused $ \focusedId -> do
@@ -21,16 +28,33 @@ centerFloat window = windows $ W.float window centerRect
 
 toggleFocusedFloat = doIfFocusedIsFloating (withFocused $ windows . W.sink) (withFocused centerFloat)
 
+myLayout = avoidStruts $ Tall 1 (3/100) (1/2)
+
+-- needed for xmobar
+myPP :: PP
+myPP = def { ppCurrent = xmobarColor "#1ABC9C" "" . wrap "[" "]"
+           , ppTitle = xmobarColor "#1ABC9C" "" . shorten 60
+           , ppVisible = wrap "(" ")"
+           , ppUrgent  = xmobarColor "red" "yellow"
+           , ppSort = getSortByXineramaPhysicalRule def
+           }
+
 main :: IO ()
-main = xmonad $ def
-	{ modMask = mod4Mask 
-	, manageHook = insertPosition End Newer 
-	}
-	`additionalKeysP`
-	[ ("M-<Return>", spawn "st") 
-	, ("M-f", spawn "firefox")
-	, ("M-S-q", kill)
-	, ("M-S-r", spawn "xmonad --recompile; xmonad --restart")
-	, ("M-S-e", spawn "killall xmonad-x86_64-linux")
-	, ("M-<Space>", toggleFocusedFloat)
-	]
+main = do 
+	xmproc <- spawnPipe "xmobar"
+	xmonad $ docks $ ewmh $ def
+		{ modMask = mod4Mask 
+		, manageHook = insertPosition End Newer 
+		, layoutHook = myLayout
+		, logHook = dynamicLogWithPP myPP { ppOutput = hPutStrLn xmproc } 
+		}
+		`additionalKeysP`
+		[ ("M-<Return>", spawn "alacritty") 
+		, ("M-f", spawn "~/projects/rofi_scripts/browser_launch.dash")
+		, ("M-a", spawn "~/projects/rofi_scripts/actions.dash")
+		, ("M-d", spawn "rofi -show drun")
+		, ("M-S-q", kill)
+		, ("M-S-r", spawn "xmonad --recompile; xmonad --restart")
+		, ("M-S-e", spawn "kill -9 -1")
+		, ("M-<Space>", toggleFocusedFloat)
+		]
